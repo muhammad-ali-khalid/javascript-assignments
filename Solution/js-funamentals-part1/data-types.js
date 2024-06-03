c6021243c63855a465b163f4827d8e5cc9bb5759
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-03 21:49:43 +0500

    Create data-types.js

---
+++ Solution/js-funamentals-part1/data-types.js
@@ -0,0 +1,10 @@
+let country = "Pakistan";
+let population = 22;
+let isIsland = false;
+let language;
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
