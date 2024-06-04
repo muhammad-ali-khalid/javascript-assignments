7430477a10bc5c8a178e6e12f191f7143d7fc34b
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-04 20:39:46 +0500

    Create introduction-to-objects.js

---
+++ Solution/js-fundamentals-part2/introduction-to-objects.js
@@ -0,0 +1,8 @@
+const myCountry = {
+  country: "Pakistan",
+  capital: "Islamabad",
+  language: "urdu",
+  population: 200,
+  neighbours: ["China", "India", "Iran", "Afghanistan"],
+};
+console.log(myCountry);
