297178edc1dc60421048ccfc1690b18cf5cc4c6a
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-04 12:52:59 +0500

    Create ternary-operator.js

---
+++ Solution/js-fundamentals-part1/ternary-operator.js
@@ -0,0 +1,14 @@
+const country = "Pakistan";
+let population = 22;
+population > 33
+  ? console.log(`${country}'s population is above average`)
+  : console.log(`${country}'s population is below average`);
+population = 13;
+population > 33
+  ? console.log(`${country}'s population is above average`)
+  : console.log(`${country}'s population is below average`);
+population = 130;
+population > 33
+  ? console.log(`${country}'s population is above average`)
+  : console.log(`${country}'s population is below average`);
+population = 22;
