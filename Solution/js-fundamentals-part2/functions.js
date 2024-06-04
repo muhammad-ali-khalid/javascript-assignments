fcf5fb9c02e7c87d1e211a408bfc152897159a44
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-04 14:13:13 +0500

    Update functions.js

--- Solution/js-fundamentals-part2/functions.js
+++ Solution/js-fundamentals-part2/functions.js
@@ -1,1 +1,7 @@
+const describeCountry = (country, population, capitalCity) =>
+  `${country} has ${population} million people and its capital city is ${capitalCity}`;
 
+const country1 = describeCountry("Pakistan", 22, "Islamabad");
+const country2 = describeCountry("India", 150, "Delhi");
+const country3 = describeCountry("Saudi Arabia", 15, "Riyadh");
+console.log(`${country1}\n${country2}\n${country3}`);
