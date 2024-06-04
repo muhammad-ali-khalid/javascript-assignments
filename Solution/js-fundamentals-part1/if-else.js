8e90fa0c2c36014d41af34ba928e3a2ce32f8b90
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-04 10:56:01 +0500

    Create if-else.js

---
+++ Solution/js-fundamentals-part1/if-else.js
@@ -0,0 +1,28 @@
+const country = "Pakistan";
+const continent = "South Asia";
+let population = 22;
+const isIsland = false;
+const language = "urdu";
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(
+    `${country}'s population is ${33 - `${population}`} below average`
+  );
+}
+population = 11;
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(
+    `${country}'s population is ${33 - `${population}`} below average`
+  );
+}
+population = 130;
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(
+    `${country}'s population is ${33 - `${population}`} below average`
+  );
+}
