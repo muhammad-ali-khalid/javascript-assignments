7aac7036097011f6974f0caa020cc6b131ce9890
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-04 20:06:17 +0500

    Update introduction-to-arrays.js

--- Solution/js-fundamentals-part2/introduction-to-arrays.js
+++ Solution/js-fundamentals-part2/introduction-to-arrays.js
@@ -1,1 +1,12 @@
-
+let populations = [1441, 220, 500, 250];
+if (populations.length == 4) console.log(true);
+else console.log(false);
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+let percentages = new Array();
+percentages.push(percentageOfWorld1(populations[0]));
+percentages.push(percentageOfWorld1(populations[1]));
+percentages.push(percentageOfWorld1(populations[2]));
+percentages.push(percentageOfWorld1(populations[3]));
+console.log(percentages);
