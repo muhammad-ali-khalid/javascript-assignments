8112257da373e871d9fda1ee1940bf43c62abf1e
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-04 10:20:53 +0500

    Create strings-and-template-literals.js

---
+++ Solution/js-fundamentals-part1/strings-and-template-literals.js
@@ -0,0 +1,7 @@
+const country = "Pakistan";
+const continent = "South Asia";
+let population = 22;
+const isIsland = false;
+const language = "urdu";
+const description = `${country} is in ${continent} and its ${population} million people speak ${language}`;
+console.log(description);
