8273fb2ed6ba34b8d14e68eb813bf25d8c614125
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-04 20:46:54 +0500

    Create dot-vs-bracket-notation.js

---
+++ Solution/js-fundamentals-part2/dot-vs-bracket-notation.js
@@ -0,0 +1,18 @@
+const myCountry = {
+  country: "Pakistan",
+  capital: "Islamabad",
+  language: "urdu",
+  population: 200,
+  neighbours: ["China", "India", "Iran", "Afghanistan"],
+};
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
+);
+myCountry.population = 202;
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
+);
+myCountry.population = 200;
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
+);
